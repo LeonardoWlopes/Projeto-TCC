aaefb6a03da44722f48158dbccc5d8ed32e24e37
Leonardo William Lopes <leonardolopessocial@gmail.com>
2021-09-02 17:54:35 -0300

    Set02 - 18:00

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function calcular(){
     let soma = 0
     for(inputnumber = 1; inputnumber <= 7;inputnumber ++){
     let inputs = document.getElementById(`input${inputnumber}`)
+
+    //Resultados finais
     soma = soma + Number(inputs.value)
     }
-    alert(soma)
+    if(soma != 0){
+    extrato()
+    }
+    
+}
+function extrato(){
+    let display = document.getElementById('resultados')
+    display.innerHTML = ''
+
 }
