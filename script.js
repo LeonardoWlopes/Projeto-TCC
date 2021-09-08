8e6c24a7d7826427e140fa9bed54a4a0f8a2da22
Leonardo William Lopes <leonardolopessocial@gmail.com>
2021-09-08 20:33:15 -0300

    Set08 - 20:30

--- script.js
+++ script.js
@@ -13,14 +13,26 @@ function calcular(){
     if(soma != 0){
     extrato(soma)
     }
+    else{
+        limpar()
+    }
     
 }
 function extrato(soma){
-    let valorkwh = 0.80
+
+    let valorkwh = 0.85
+    let valorhora = soma*valorkwh
+    let valordia = valorhora * 24
+    let valorfinal = valordia * 30
+
     let display = document.getElementById('resultados')
     display.innerHTML = ''
     display.innerHTML = `<h3>Total sendo gasto: ${soma}Kwh</h3>`
-
-    display.innerHTML += `<h3>Total sendo gasto R$ ${soma*valorkwh}</h3>`
-
+    display.innerHTML += `<h3>Valor por hora: <span>R$ ${valorhora.toFixed(2)}</span></h3>`
+    display.innerHTML += ` <h3>Valor em 24 horas<span>R$ ${valordia.toFixed(2)}</span></h3>`
+    display.innerHTML += `<h3>Valor mensal: <span>R$ ${valorfinal.toFixed(2)}</span></h3>`
+}
+function limpar(){
+    let display = document.getElementById('resultados')
+    display.innerHTML = ''
 }
