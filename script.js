042a245304f398011b50870c47a651966c6aa1bc
Leonardo William Lopes <leonardolopessocial@gmail.com>
2021-09-08 09:07:06 -0300

    Set08 - 9:05

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+
 function calcular(){
+    //Somar os valores
     let soma = 0
     for(inputnumber = 1; inputnumber <= 7;inputnumber ++){
     let inputs = document.getElementById(`input${inputnumber}`)
@@ -6,13 +8,19 @@ function calcular(){
     //Resultados finais
     soma = soma + Number(inputs.value)
     }
+
+    //chamada para extrato
     if(soma != 0){
-    extrato()
+    extrato(soma)
     }
     
 }
-function extrato(){
+function extrato(soma){
+    let valorkwh = 0.80
     let display = document.getElementById('resultados')
     display.innerHTML = ''
+    display.innerHTML = `<h3>Total sendo gasto: ${soma}Kwh</h3>`
+
+    display.innerHTML += `<h3>Total sendo gasto R$ ${soma*valorkwh}</h3>`
 
 }
