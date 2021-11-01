cbb174a4843d98200607fc4b5cd88f4911663276
Leonardo William Lopes <62454440+LeonardoWlopes@users.noreply.github.com>
2021-10-31 21:56:02 -0300

    Out31 - 21:55

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  margin: auto;
+  align-items: center;
+  justify-content: center;
+  background-color: yellow;
+  width: 100%;
+  height: 100%;
+  
+`;
